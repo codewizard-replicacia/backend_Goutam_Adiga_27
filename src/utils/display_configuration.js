ff1404ea1e39c67b5fe473816ffd32e1ac91f817
RibhuGuha <ribhu@replicacia.com>
2023-09-06 11:43:09 +0530

    initial commit

---
+++ src/utils/display_configuration.js
@@ -0,0 +1,27 @@
+export const routeViewConfig = {
+Details: [
+    {
+      key: "StartedFrom",
+      value: "StartedFrom",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "RouteName",
+      value: "RouteName",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "RouteId",
+      value: "RouteId",
+      type: "text",
+      editable: false,
+      required: true
+    },
+  ],
+Type: [
+  ],
+}
